2a04673cd147b55f098c828c8f99f14baaaa3c89
Jordan Ov <jordan@ov.tech>
2026-06-20 15:05:18 +0300

    fix: sitemap TS error + upgrade to Next.js 16 / React 19

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -35,8 +35,7 @@ export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.map(u => `  <url>
-    <loc>${u.loc}</loc>
-    ${u.lastmod ? `<lastmod>${u.lastmod}</lastmod>` : ''}
+    <loc>${u.loc}</loc>${'lastmod' in u && u.lastmod ? `\n    <lastmod>${u.lastmod}</lastmod>` : ''}
     <changefreq>${u.changefreq}</changefreq>
     <priority>${u.priority}</priority>
   </url>`).join('\n')}
